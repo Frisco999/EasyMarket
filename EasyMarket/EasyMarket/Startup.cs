963a3d6b94db463cbfe3e15c7eabc2bfd06e8415
Frisco <Freeman99999@narod.ru>
2014-05-15 20:39:10 +0300

    Creating Project.

---
+++ EasyMarket/EasyMarket/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EasyMarket.Startup))]
+namespace EasyMarket
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
