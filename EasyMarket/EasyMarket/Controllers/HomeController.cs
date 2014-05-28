1514302e2bd74809b065ac5d20a862d06c177237
kit <alina.nor.mail@gmail.com>
2014-05-28 22:05:53 +0300

    Создана авторизация.

--- EasyMarket/EasyMarket/Controllers/HomeController.cs
+++ EasyMarket/EasyMarket/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using EasyMarket.Models;
+using System.Web.Security;
+using System.Web.UI;
 
 namespace EasyMarket.Controllers
 {
@@ -12,9 +14,45 @@ namespace EasyMarket.Controllers
         Easy_MarketEntities db = new Easy_MarketEntities();
         public ActionResult Index()
         {
-
+            if (Request.Cookies[FormsAuthentication.FormsCookieName] != null) { ViewBag.autorized = true; }
+            else
+            {
+                ViewBag.Autorized = false;
+            }
             return View();
         }
+
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public ActionResult Autorization(User user)
+        {
+           
+            if (user != null)
+            {
+                User currentUser = db.Users.FirstOrDefault(x => x.email == user.email && x.password == user.password);
+                if (currentUser != null)
+                {
+                    FormsAuthentication.SetAuthCookie(currentUser.email, user.rememberme);
+                    //HttpCookie authCookie = Request.Cookies[FormsAuthentication.FormsCookieName];
+                    //FormsAuthenticationTicket ticket = FormsAuthentication.Decrypt(authCookie.Value);
+                    
+                }  
+                        return RedirectToAction("Index", "Home");
+                 
+        }
+            
+
+
+
+    
+            return View();
+        
+}
 
         public ActionResult About()
         {
