f48628a72dda8a0e26e243516e08151da5021c46
kit <alina.nor.mail@gmail.com>
2014-05-30 21:52:14 +0300

    Добавлено отображение данных о товаре из базы двнных.

--- EasyMarket/EasyMarket/Controllers/HomeController.cs
+++ EasyMarket/EasyMarket/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 using EasyMarket.Models;
 using System.Web.Security;
 using System.Web.UI;
+using System.Data.Entity;
+using EasyMarket.Classes;
 
 namespace EasyMarket.Controllers
 {
@@ -59,16 +61,33 @@ namespace EasyMarket.Controllers
 
         public ActionResult Goods()
         {
-     
+            List<ItemInfo> itemsInfo = new List<ItemInfo>();
+            List<Item> items = new List<Item>(db.Items);
+            foreach (Item item in items)
+            {
+                List<Image> images = new List<Image>(db.Images.Where(i => i.item == item.id_item));
+                List<Size> sizes = new List<Size>(db.Sizes.Where(s => s.item == item.id_item));
+                itemsInfo.Add(new ItemInfo { Images = images, Sizes = sizes, Item = item });
+            }
 
-            return View();
+            return View(itemsInfo);
+
         }
 
         public ActionResult NewGoods()
         {
 
+            List<ItemInfo> itemsInfo = new List<ItemInfo>();
+            List<Item> items = new List<Item>(db.Items);
+            items = items.Skip(items.Count()-3).ToList();
+            foreach (Item item in items)
+            {
+                List<Image> images = new List<Image>(db.Images.Where(i => i.item == item.id_item));
+                List<Size> sizes = new List<Size>(db.Sizes.Where(s => s.item == item.id_item));
+                itemsInfo.Add(new ItemInfo { Images = images, Sizes = sizes, Item = item });
+            }
 
-            return View();
+            return View(itemsInfo);
         }
 
         public ActionResult Graphical_Search()
@@ -92,46 +111,91 @@ namespace EasyMarket.Controllers
             return View();
         }
 
+        
         public ActionResult Tshirts()
         {
+            List<ItemInfo> itemsInfo = new List<ItemInfo>();
+            List<Item> items = new List<Item>(db.Items.Where(m => m.type == "tshirt"));
+            foreach (Item item in items)
+            {
+                List<Image> images = new List<Image>(db.Images.Where(i => i.item == item.id_item));
+                List<Size> sizes = new List<Size>(db.Sizes.Where(s => s.item == item.id_item));
+                itemsInfo.Add(new ItemInfo { Images = images, Sizes = sizes, Item = item });
+            }
 
-
-            return View();
+            return View(itemsInfo);
         }
+
 
         public ActionResult Singlets()
         {
+            List<ItemInfo> itemsInfo = new List<ItemInfo>();
+            List<Item> items = new List<Item>(db.Items.Where(m => m.type == "singlet"));
+            foreach (Item item in items)
+            {
+                List<Image> images = new List<Image>(db.Images.Where(i => i.item == item.id_item));
+                List<Size> sizes = new List<Size>(db.Sizes.Where(s => s.item == item.id_item));
+                itemsInfo.Add(new ItemInfo { Images = images, Sizes = sizes, Item = item });
+            }
 
-
-            return View();
+            return View(itemsInfo);
         }
 
         public ActionResult Shirts()
         {
+            List<ItemInfo> itemsInfo = new List<ItemInfo>();
+            List<Item> items = new List<Item>(db.Items.Where(m => m.type == "shirt"));
+            foreach (Item item in items)
+            {
+                List<Image> images = new List<Image>(db.Images.Where(i => i.item == item.id_item));
+                List<Size> sizes = new List<Size>(db.Sizes.Where(s => s.item == item.id_item));
+                itemsInfo.Add(new ItemInfo { Images = images, Sizes = sizes, Item = item });
+            }
 
+            return View(itemsInfo);
 
-            return View();
         }
 
         public ActionResult Jumpers()
         {
+            List<ItemInfo> itemsInfo = new List<ItemInfo>();
+            List<Item> items = new List<Item>(db.Items.Where(m => m.type == "cardigan"));
+            foreach (Item item in items)
+            {
+                List<Image> images = new List<Image>(db.Images.Where(i => i.item == item.id_item));
+                List<Size> sizes = new List<Size>(db.Sizes.Where(s => s.item == item.id_item));
+                itemsInfo.Add(new ItemInfo { Images = images, Sizes = sizes, Item = item });
+            }
 
-
-            return View();
+            return View(itemsInfo);
         }
 
         public ActionResult Polos()
         {
+            List<ItemInfo> itemsInfo = new List<ItemInfo>();
+            List<Item> items = new List<Item>(db.Items.Where(m => m.type == "polo"));
+            foreach (Item item in items)
+            {
+                List<Image> images = new List<Image>(db.Images.Where(i => i.item == item.id_item));
+                List<Size> sizes = new List<Size>(db.Sizes.Where(s => s.item == item.id_item));
+                itemsInfo.Add(new ItemInfo { Images = images, Sizes = sizes, Item = item });
+            }
 
-
-            return View();
+            return View(itemsInfo);
         }
 
             public ActionResult Hoodies()
         {
-            
+            List<ItemInfo> itemsInfo = new List<ItemInfo>();
+            List<Item> items = new List<Item>(db.Items.Where(m => m.type == "smock"));
+            foreach (Item item in items)
+            {
+                List<Image> images = new List<Image>(db.Images.Where(i => i.item == item.id_item));
+                List<Size> sizes = new List<Size>(db.Sizes.Where(s => s.item == item.id_item));
+                itemsInfo.Add(new ItemInfo { Images = images, Sizes = sizes, Item = item });
+            }
 
-            return View();
+            return View(itemsInfo);
         }
 
         
